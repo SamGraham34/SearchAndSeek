a7e10f09d4c8e742bb5accf373a009cbf49aa4a2
SamGraham34 <47090256+SamGraham34@users.noreply.github.com>
2019-02-07 07:51:35 -0500

    Add files via upload
    Initial Commit

---
+++ SeekingSearcher/src/main/java/SearchEngine/Searcher.java
@@ -0,0 +1,11 @@
+/*
+ * Some license info may be nice
+ */
+package SearchEngine;
+
+/**
+ *Search Engine Project COP 2805. Edit this later.
+ */
+public class Searcher {
+    // We will put some code here if we want
+}
